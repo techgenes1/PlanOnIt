5b19d30ab1102b7dc359487b9559fc9be2c886ce
techgenes1 <techgenes90@gmail.com>
2018-10-02 13:14:23 +0530

    Initial

---
+++ src/components/Header/HeaderLinks.jsx
@@ -0,0 +1,90 @@
+/*eslint-disable*/
+import React from "react";
+// react components for routing our app without refresh
+import { Link } from "react-router-dom";
+
+// @material-ui/core components
+import withStyles from "@material-ui/core/styles/withStyles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import Tooltip from "@material-ui/core/Tooltip";
+
+// @material-ui/icons
+import { Apps, CloudDownload } from "@material-ui/icons";
+
+// core components
+import CustomDropdown from "components/CustomDropdown/CustomDropdown.jsx";
+import Button from "components/CustomButtons/Button.jsx";
+
+import headerLinksStyle from "assets/jss/material-kit-react/components/headerLinksStyle.jsx";
+
+function HeaderLinks({ ...props }) {
+  const { classes } = props;
+  return (
+    <List className={classes.list}>
+      <ListItem className={classes.listItem}>
+        <CustomDropdown
+          noLiPadding
+          buttonText="Events"
+          buttonProps={{
+            className: classes.navLink,
+            color: "transparent"
+          }}
+          buttonIcon={Apps}
+          dropdownList={[
+            <Link to="/" className={classes.dropdownLink}>
+              Weddings
+            </Link>,
+            <Link to="/" className={classes.dropdownLink}>
+              Birthday parties
+            </Link>,
+            <Link to="/" className={classes.dropdownLink}>
+              Meeting
+            </Link>,
+            <a
+              href="https://creativetimofficial.github.io/material-kit-react/#/documentation"
+              target="_blank"
+              className={classes.dropdownLink}
+            >
+              Make your own
+            </a>
+
+          ]}
+        />
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <Button
+          href="https://www.creative-tim.com/product/material-kit-react"
+          color="transparent"
+          target="_blank"
+          className={classes.navLink}
+        >
+          <CloudDownload className={classes.icons} /> About Us
+        </Button>
+      </ListItem>
+
+      <ListItem className={classes.listItem}>
+        <Button
+          href="https://www.creative-tim.com/product/material-kit-react"
+          color="transparent"
+          target="_blank"
+          className={classes.navLink}
+        >
+          <CloudDownload className={classes.icons} /> Become a partner
+        </Button>
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <Button
+          href="https://www.creative-tim.com/product/material-kit-react"
+          color="transparent"
+          target="_blank"
+          className={classes.navLink}
+        >
+         Sign in
+        </Button>
+      </ListItem>
+    </List>
+  );
+}
+
+export default withStyles(headerLinksStyle)(HeaderLinks);
